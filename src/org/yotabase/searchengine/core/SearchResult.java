7cc5d0f584203be5f11e7ea82f807bd0a9ec8efa
Christian Vadalà <chr.vadala@gmail.com>
2015-03-22 22:20:14 +0100

    aggiunge struttura del core

---
+++ src/org/yotabase/searchengine/core/SearchResult.java
@@ -0,0 +1,49 @@
+package org.yotabase.searchengine.core;
+
+import java.util.List;
+
+public class SearchResult {
+
+	private Integer resultsCount;
+	
+	private Integer page;
+	
+	private List<Snippet> snippets;
+	
+	private List<String> suggestedSearch;
+
+	public Integer getResultsCount() {
+		return resultsCount;
+	}
+
+	public void setResultsCount(Integer resultsCount) {
+		this.resultsCount = resultsCount;
+	}
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
+	public List<Snippet> getSnippets() {
+		return snippets;
+	}
+
+	public void setSnippets(List<Snippet> snippets) {
+		this.snippets = snippets;
+	}
+
+	public List<String> getSuggestedSearch() {
+		return suggestedSearch;
+	}
+
+	public void setSuggestedSearch(List<String> suggestedSearch) {
+		this.suggestedSearch = suggestedSearch;
+	}
+	
+	
+	
+}
