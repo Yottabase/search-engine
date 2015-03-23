86cc1c2d94bc7d6512b637ad900892dadd9f6271
Christian Vadalà <chr.vadala@gmail.com>
2015-03-23 14:28:34 +0100

    aggiunge stub ed apiSearcher

--- src/org/yottabase/eureka/ui/web/action/ApiSearchAction.java
+++ src/org/yottabase/eureka/ui/web/action/ApiSearchAction.java
@@ -6,8 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.yottabase.eureka.core.SearchResult;
+import org.yottabase.eureka.core.Searcher;
+import org.yottabase.eureka.core.WebPageSearchResult;
 import org.yottabase.eureka.ui.web.core.Action;
+import org.yottabase.eureka.ui.web.stub.StubSearcher;
 
 public class ApiSearchAction implements Action{
 
@@ -15,9 +20,28 @@ public class ApiSearchAction implements Action{
 	public void run(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		Searcher searcher = new StubSearcher();
+		SearchResult result = searcher.search("hello query", 1, 10);
+		
 		JSONObject json = new JSONObject();
 		
-		json.put("Hello", "World");
+		json.put("page", result.getPage());
+		json.put("itemInPage", result.getItemInPage());
+		json.put("itemsCount", result.getItemsCount());
+		json.put("queryResponseTime", result.getQueryResponseTime());
+		
+		JSONArray items = new JSONArray();
+		json.put("webPageSnippets", items);
+		
+		for(WebPageSearchResult webPage : result.getWebPageSnippets()){
+			JSONObject item = new JSONObject();
+			item.put("title", webPage.getTitle());
+			item.put("url", webPage.getUrl());
+			item.put("snippet", webPage.getSnippet());
+			//item.put("skippedWords", new JSONArray());
+			item.put("date", webPage.getDate());
+			items.put(item);
+		}
 		
 		response.setContentType("application/json");
 		response.getWriter().write(json.toString());
---
+++ src/org/yottabase/eureka/ui/web/stub/StubSearcher.java
@@ -0,0 +1,56 @@
+package org.yottabase.eureka.ui.web.stub;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.yottabase.eureka.core.SearchResult;
+import org.yottabase.eureka.core.Searcher;
+import org.yottabase.eureka.core.WebPageSearchResult;
+
+public class StubSearcher implements Searcher{
+
+	@Override
+	public SearchResult search(String query, Integer page, Integer itemInPage) {
+		
+		List<WebPageSearchResult> webPages = new ArrayList<WebPageSearchResult>();
+		webPages.add(new WebPageSearchResult(
+			"Christian", 
+			"Ciao sono uno snippet", 
+			"http://www.google.it", 
+			new ArrayList<String>(),
+			new Date()
+		));
+		
+		webPages.add(new WebPageSearchResult(
+			"Leonardo", 
+			"Ciao sono un professor snippet ", 
+			"http://www.google.it/leonardo.proff", 
+			new ArrayList<String>(),
+			new Date()
+		));
+		
+		webPages.add(new WebPageSearchResult(
+			"Alessandro", 
+			"Ciao sono uno snippet assistente del proff Leonardo", 
+			"http://www.google.it/leonardo.proff/alessandro", 
+			new ArrayList<String>(),
+			new Date()
+		));
+		
+		SearchResult result = new SearchResult();
+		result.setPage(page);
+		result.setItemInPage(itemInPage);
+		result.setItemsCount(3);
+		result.setQueryResponseTime(0.45);
+		result.setWebPageSnippets(webPages);
+		return result;
+	}
+
+	@Override
+	public List<String> autocomplete(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
