bd8e808d02f456c7ce92437eea451da015c9d3bd
Christian Vadalà <chr.vadala@gmail.com>
2015-03-24 16:26:49 +0100

    code clean

--- WebContent/assets/js/scripts.js
+++ WebContent/assets/js/scripts.js
@@ -18,31 +18,33 @@ jQuery( document ).ready(function( $ ) {
 			
 			console.log(data);
 			
-			$('#stats').text("Circa "+data.itemsCount+" risultati ("+data.queryResponseTime+" secondi)");
+			//elimina vecchi risultati
+			resultsBlock.fadeOut(200);
 			resultsBlock.empty();
 			resultsBlock.stop(true, true);
-			resultsBlock.fadeOut(200);
+			$('#suggested-search').hide();
+			$('#suggested-search .words').empty();
 			
+			//aggiorna statistiche
+			$('#stats').text("Circa "+data.itemsCount+" risultati ("+data.queryResponseTime+" secondi)");
+			
+			//disegna nuovi blocchi
 			data.webPages.forEach(function(item){
-				
 				var resultBlock = webPageTemplate.clone();
 				resultBlock.find('.title a').text(item.title);
 				resultBlock.find('.snippet').text(item.snippet);
 				resultBlock.find('.url a').text(item.url);
 				resultBlock.find('a').prop('href',item.url);
-				
 				resultsBlock.append(resultBlock);
-				
-				
 			});
 			
-			$('#suggested-search .words').empty();
+			//aggiunge parole suggerite
 			data.suggestedSearch.forEach(function(item){
 				$('#suggested-search .words').append('<a href="#">'+item+'</a>');
 			});
 			$('#suggested-search').show();
 			
-			
+			//effetua lo swith alla modalità risultati
 			if(! hasResults){
 				hasResults = true;
 				$('#brand img').fadeOut(500, function(){
@@ -56,6 +58,8 @@ jQuery( document ).ready(function( $ ) {
 					});
 				});
 			}
+			
+			//mostra risultati
 			resultsBlock.fadeIn(500);
 			
 		});
