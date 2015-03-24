6e3abbc8e18d86eaf7b4b7aa0459e1c81a1a2a62
Christian Vadalà <chr.vadala@gmail.com>
2015-03-24 15:20:25 +0100

    rimosso metodo addInputPath

--- src/org/yottabase/eureka/core/InputManager.java
+++ src/org/yottabase/eureka/core/InputManager.java
@@ -3,16 +3,6 @@ package org.yottabase.eureka.core;
 public interface InputManager {
 	
 	/**
-	 * Aggiunge un path di 
-	 * 	- un file .warc 
-	 * 	- un archivio GZip contente un file .warc
-	 * 	- una cartella contente files .warc e/o archivi contenenti ciascuno un file .warc
-	 * 
-	 * @param path
-	 */
-	public void addInputPath(String path);
-	
-	/**
 	 * Restituisce la prossima WebPage se presente nel complesso dei file .warc di input,
 	 * null altrimenti.
 	 * 
