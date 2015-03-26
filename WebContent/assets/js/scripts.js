9e1bf789e1e5d589c0456c0cc0214a926612bab7
Christian Vadalà <chr.vadala@gmail.com>
2015-03-26 21:30:26 +0100

    javascript refactoring

--- WebContent/assets/js/scripts.js
+++ WebContent/assets/js/scripts.js
@@ -1,84 +1,28 @@
+
+/*** env ***/
+var resultsMode = false;
+var webPageTemplate = $(' \
+	<div class="web-page"> \
+		<div class="title"><a href="#">Prova</a></div> \
+		<div class="snippet">La mia descrizione</div> \
+		<div class="url"><a href="#"></a></div> \
+	</div>');
+
+
+/*** gestione ricerca ***/
 jQuery( document ).ready(function( $ ) {
-
-	var resultsBlock = $('#results');
-	
-	var webPageTemplate = $(' \
-		<div class="web-page"> \
-			<div class="title"><a href="#">Prova</a></div> \
-			<div class="snippet">La mia descrizione</div> \
-			<div class="url"><a href="#"></a></div> \
-		</div>');
-	
-	
-	var hasResults = false;
-	
 	$('#search-input').on('input', function(){
-		
 		var q = $(this).val();
-		
-		$.get("apiSearch.do", {"q" : q, "p" : 1}).done( function( data ) {
-			
-			console.log(data);
-			
-			//elimina vecchi risultati
-			resultsBlock.fadeOut(200);
-			resultsBlock.empty();
-			resultsBlock.stop(true, true);
-			$('#suggested-search').hide();
-			$('#suggested-search .words').empty();
-			
-			//aggiorna statistiche
-			$('#stats').text("Circa "+data.itemsCount+" risultati ("+data.queryResponseTime+" secondi)");
-			
-			//disegna nuovi blocchi
-			data.webPages.forEach(function(item){
-				
-				//mette in grassetto le parole degli snippets presenti nella query
-				var snippet = item.snippet;
-				item.highlights.forEach(function(w){
-					snippet = highlightWords(snippet, w);
-				});
-				
-				//crea item di output
-				var resultBlock = webPageTemplate.clone();
-				resultBlock.find('.title a').text(item.title);
-				resultBlock.find('.snippet').html(snippet);
-				resultBlock.find('.url a').text(item.url);
-				resultBlock.find('a').prop('href',item.url);
-				
-				resultsBlock.append(resultBlock);
-			});
-			
-			
-			//aggiunge parole suggerite
-			data.suggestedSearch.forEach(function(item){
-				$('#suggested-search .words').append('<a href="#">'+item+'</a>');
-			});
-			$('#suggested-search').show();
-			
-			//effetua lo swith alla modalità risultati
-			if(! hasResults){
-				hasResults = true;
-				$('#brand img').fadeOut(500, function(){
-					
-					$('#brand').animate({
-						  height: 0,
-					}, 300, function(){
-						$('#brand img').removeClass('center-block');
-						$('#brand').addClass('little');
-						$('#brand img').fadeIn(200);
-					});
-				});
-			}
-			
-			//mostra risultati
-			resultsBlock.fadeIn(500);
-			
-		});
+		clearResults();
+		if(q.length != 0){
+			performQuery(q);	
+		}
 	});
+});
 
-	
-	//begin TYPEAHEAD
+/*** autocompletamento ***/
+jQuery( document ).ready(function( $ ) {	
+
 	var suggestions = new Bloodhound({
 	  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
 	  queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -98,7 +42,7 @@ jQuery( document ).ready(function( $ ) {
 	//end TYPEAHEAD
 });
 
-/*** funzione per highlightWords ***/
+/*** aggiunge highlightWords ***/
 function highlightWords( line, word )
 {
      var regex = new RegExp( '(' + word + ')', 'gi' );
@@ -116,5 +60,85 @@ Array.prototype.remove = function() {
     }
     return this;
 };
+
+/*** esegue query e disegna risultati ***/
+function performQuery(query, page){
+	
+	$.get("apiSearch.do", {
+		"q" : query, 
+		"page" : page
+		}).done( function( data ) {
+		
+		console.log(data);
+		
+		var resultsBlock = $('#results');
+		$('#stats').text("Circa "+data.itemsCount+" risultati ("+data.queryResponseTime+" secondi)");
+		
+		//disegna nuovi blocchi
+		data.webPages.forEach(function(item){
+			//mette in grassetto le parole degli snippets presenti nella query
+			var snippet = item.snippet;
+			item.highlights.forEach(function(w){
+				snippet = highlightWords(snippet, w);
+			});
+			
+			//crea item di output
+			var resultBlock = webPageTemplate.clone();
+			resultBlock.find('.title a').text(item.title);
+			resultBlock.find('.snippet').html(snippet);
+			resultBlock.find('.url a').text(item.url);
+			resultBlock.find('a').prop('href',item.url);
+			resultsBlock.append(resultBlock);
+		});
+		
+		//aggiunge ricerche suggerite
+		if(data.suggestedSearch.length > 0){
+			data.suggestedSearch.forEach(function(item){
+				$('#suggested-search .words').append('<a href="#">'+item+'</a>');
+			});
+			$('#suggested-search').show();
+		}
+		
+		if(! resultsMode ){ gotoResultsMode(); }
+		resultsBlock.fadeIn(500);
+		
+	});
+}
+
+
+//effetua lo swith alla modalità risultati
+function gotoResultsMode(){
+	var resultsBlock = $('#results');
+	
+	resultsMode = true;
+	$('#brand img').fadeOut(500, function(){
+		
+		$('#brand').animate({
+			  height: 0,
+		}, 300, function(){
+			$('#brand img').removeClass('center-block');
+			$('#brand').addClass('little');
+			$('#brand img').fadeIn(200);
+		});
+	});
+}
+
+
+function clearResults(){
+	//elimina vecchi risultati
+	var resultsBlock = $('#results');
+	resultsBlock.fadeOut(200);
+	resultsBlock.empty();
+	resultsBlock.stop(true, true);
+	$('#suggested-search').hide();
+	$('#suggested-search .words').empty();
+	
+	//aggiorna statistiche
+	$('#stats').text("");
+};
+
+
+
+
 
 //END
