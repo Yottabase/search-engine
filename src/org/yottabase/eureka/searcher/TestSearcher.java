e8130f9b83d307cce7c660249eacd3d66b865eb8
hduser <hduser@leonardo-K53SD>
2015-03-26 17:45:34 +0100

    Refacoring

--- src/org/yottabase/eureka/searcher/TestSearcher.java
+++ src/org/yottabase/eureka/searcher/TestSearcher.java
@@ -1,42 +1,40 @@
 package org.yottabase.eureka.searcher;
 
-import java.io.IOException;
-
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.search.ScoreDoc;
 import org.yottabase.eureka.core.SearchResult;
 import org.yottabase.eureka.core.WebPageSearchResult;
 
 public class TestSearcher {
 
-	public static void main(String[] args) throws IOException, ParseException, java.text.ParseException {
+	public static void main(String[] args) {
 		/*
 		 * richiamo la classe SearchIndex per testare il funzionamento
 		 * successivamente sarà sostituita dalla chiamata del controller
 		 */
+		Integer a; Float b;
 
 		IndexSearch searcher = new IndexSearch();
 		SearchResult result = new SearchResult();
 
-		result = searcher.search("the", 1, 20);
+		result = searcher.search("the", 1, 10, null, null);
+		a = result.getDocID();
+		b = result.getDocScore();
 		
 		System.out.println(result.getItemsCount() + " risultati\n");
-		System.out.println("Suggested searches: " + result.getSuggestedSearch().toString() + "\n");
+		System.out.println("Suggested searches: " + result.getSuggestedSearches().toString() + "\n");
 		for (WebPageSearchResult page : result.getWebPages())
 			System.out.println( page.toString() + "\n" );
 		
-		/**
+
 		System.out.println("-----------------------------------");
 		
 		searcher = new IndexSearch();
 		result = new SearchResult();
-		searcher.lastScore = new ScoreDoc(9277, 0.38888752f);
-		result = searcher.search("the", 2, 10);
+		result = searcher.search("the", 2, 10, a, b);
 		
 		System.out.println(result.getItemsCount() + " risultati\n");
-		System.out.println("Suggested searches: " + result.getSuggestedSearch().toString() + "\n");
+		System.out.println("Suggested searches: " + result.getSuggestedSearches().toString() + "\n");
 		for (WebPageSearchResult page : result.getWebPages())
 			System.out.println( page.toString() + "\n" );
-		**/
+
 	}
 }
